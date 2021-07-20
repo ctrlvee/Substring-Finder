2022d2cd76e84d6c80d5159f0cbeb92470d6cc49
ctrlVee <me123opportunity@gmail.com>
2021-07-19 21:36:10 -0400

    Fully functional

--- script.rb
+++ script.rb
@@ -1,3 +1,26 @@
 require 'pry-byebug'
 
-dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+@dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+@main_list = []
+
+def getUserInput
+  @string = gets.chomp
+end
+
+
+def find_match
+  getUserInput()
+  @string.split.each do |substring|
+    temp_list = @dictionary.find_all do |dict_word| 
+    dict_word.include?(substring)
+    
+    end
+    @main_list.concat(temp_list) 
+    binding.pry 
+  end
+  @main_list
+  
+end
+
+find_match()
